0c5fc4b8cdc02a02272120ce184d9f6be0d31c57
Qimeng Yang <90386459+yangqimeng99@users.noreply.github.com>
2024-05-08 09:33:13 +0800

    add install.sh

---
+++ install.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+PYTHON_PATH=$(realpath $(which python3))
+
+if [ -z "$PYTHON_PATH" ]; then
+    echo "Python3 is not installed or not found in PATH."
+    exit 1
+fi
+
+sed -i "1s|#!.*python3|#!$PYTHON_PATH|" ./bin/svlearn
+
+find ./bin/ -type f -exec chmod 755 {} \;
+
+echo "SVLearn has been Successfully installed!"
